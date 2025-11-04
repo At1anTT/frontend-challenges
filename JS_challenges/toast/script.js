e28ba5082746d9fce446609c49d9635247ca46c2
At1anTT <daniil_kiselev@list.ru>
2025-11-04 22:05:54 +0300

    finish interface

---
+++ JS_challenges/toast/script.js
@@ -0,0 +1,11 @@
+const horisontalPosition = document.getElementById(
+  'horisontal-position-selector'
+);
+const verticalPosition = document.getElementById('vertical-position-selector');
+
+const typeWindow = document.getElementById('type-selector');
+
+const messageForWindow = document.getElementById('toast-message-input');
+
+const durrationValue = document.getElementById('range-input');
+
