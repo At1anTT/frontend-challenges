5c4d3494dd109c6369241aaf8d38762d2107b254
At1anTT <daniil_kiselev@list.ru>
2025-11-19 09:56:47 -0500

    add js script

--- JS_challenges/star_rating/script.js
+++ JS_challenges/star_rating/script.js
@@ -1,6 +1,22 @@
 // 😢😞😐😀😎
 
 
+const starsMap = document.getElementsByClassName('star')
+const starWrapper = document.getElementById('star-wrapper')
+
+starWrapper.addEventListener('focus', (event) => setGoldColor(event), true)
+
+function setGoldColor(event) {
+    const target = event.target;
+    
+    const targetIndex= +target.getAttribute('data-index');
+
+
+    for (let i= targetIndex; i >= 0; i--) {
+        starsMap[i].classList.add('star-filled');
+    }
+}
+
 
 
 
